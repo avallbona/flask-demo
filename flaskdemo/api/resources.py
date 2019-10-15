43738a87412f56eeca772388b58c1422f510957c
Andreu Vallbona Plazas <avallbona@gmail.com>
2019-10-15 21:17:37 +0200

    added more methods

--- flaskdemo/api/resources.py
+++ flaskdemo/api/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from flaskdemo import db
 from flaskdemo.models import Post
@@ -12,7 +12,7 @@ class PostResource(Resource):
 
     def get(self, post_id):
         post = get_or_404(post_id)
-        result = {'id': post.id, 'title': post.title}
+        result = {'id': post.id, 'title': post.title, 'content': post.content}
         return result
 
     def delete(self, post_id):
@@ -22,8 +22,15 @@ class PostResource(Resource):
         return {}, 204
 
     def put(self, post_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str, help='Title of the post')
+        parser.add_argument('content', type=str, help='Content of the post')
+        args = parser.parse_args()
         post = get_or_404(post_id=post_id)
-        pass
+        post.title = args.get('title')
+        post.content = args.get('content')
+        db.session.commit()
+        return {'id': post_id, 'title': post.title, 'content': post.content}
 
 
 class PostListResource(Resource):
@@ -34,4 +41,12 @@ class PostListResource(Resource):
         return result
 
     def post(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str, help='Title of the post')
+        parser.add_argument('content', type=str, help='Content of the post')
+        parser.add_argument('user_id', type=int, help='Id of the user')
+        args = parser.parse_args()
+        post = Post(title=args.get('title'), content=args.get('content'), user_id=args.get('user_id'))
+        db.session.add(post)
+        db.session.commit()
+        return {'id': post.id, 'title': post.title, 'content': post.content}
