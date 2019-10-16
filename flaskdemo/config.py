1b0ec52790b139a96bac29adab9d1348cec5ef68
Andreu Vallbona Plazas <avallbona@gmail.com>
2019-10-16 15:51:33 +0200

    minor changes

--- flaskdemo/config.py
+++ flaskdemo/config.py
@@ -5,7 +5,6 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'sqlite.db')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
         os.environ.get('DB_USER'),
