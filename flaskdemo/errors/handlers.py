7043ac4171a247bdf38dd39662c5b0fc3f1ffba9
Andreu Vallbona Plazas <avallbona@gmail.com>
2019-10-17 13:37:52 +0200

    refactored errors templates location

--- flaskdemo/errors/handlers.py
+++ flaskdemo/errors/handlers.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, render_template
 
-errors = Blueprint('errors', __name__,
-                   template_folder='templates')
+errors = Blueprint('errors', __name__, template_folder='templates')
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    # return render_template('errors/404.html'), 404
-    return render_template('errors/404_custom.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @errors.app_errorhandler(403)
